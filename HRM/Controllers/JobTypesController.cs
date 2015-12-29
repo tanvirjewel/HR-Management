2ea8bceae33ecfa4b3750ead6a4b8e744ae1ac50
tanvirjewel <tanvirjewel@gmail.com>
2015-12-29 22:21:53 +0600

    test

---
+++ HRM/Controllers/JobTypesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HRM.Models;
+
+namespace HRM.Controllers
+{
+    public class JobTypesController : Controller
+    {
+        private HrDBEntities1 db = new HrDBEntities1();
+
+        // GET: JobTypes
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.JobTypes.ToListAsync());
+        }
+
+        // GET: JobTypes/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobType jobType = await db.JobTypes.FindAsync(id);
+            if (jobType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobType);
+        }
+
+        // GET: JobTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: JobTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name")] JobType jobType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.JobTypes.Add(jobType);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(jobType);
+        }
+
+        // GET: JobTypes/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobType jobType = await db.JobTypes.FindAsync(id);
+            if (jobType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobType);
+        }
+
+        // POST: JobTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name")] JobType jobType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jobType).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(jobType);
+        }
+
+        // GET: JobTypes/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobType jobType = await db.JobTypes.FindAsync(id);
+            if (jobType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobType);
+        }
+
+        // POST: JobTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            JobType jobType = await db.JobTypes.FindAsync(id);
+            db.JobTypes.Remove(jobType);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
