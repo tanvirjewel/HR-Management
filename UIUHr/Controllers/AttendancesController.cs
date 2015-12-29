f29061ab9c00bd3835cf438dab000b723aa714ce
tanvirjewel <tanvirjewel@gmail.com>
2015-12-30 02:29:22 +0600

    New file added !!!

---
+++ UIUHr/Controllers/AttendancesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UIUHr.Models;
+
+namespace UIUHr.Controllers
+{
+    public class AttendancesController : Controller
+    {
+        private HrDBEntities db = new HrDBEntities();
+
+        // GET: Attendances
+        public async Task<ActionResult> Index()
+        {
+            var attendances = db.Attendances.Include(a => a.Employee);
+            return View(await attendances.ToListAsync());
+        }
+
+        // GET: Attendances/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attendance attendance = await db.Attendances.FindAsync(id);
+            if (attendance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(attendance);
+        }
+
+        // GET: Attendances/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName");
+            string date = string.Empty;
+            try
+            {
+                string time = DateTime.Now.ToString();
+                string[] phase = time.Split(' ');
+                ViewBag.date = phase[0];
+                date = phase[0];
+                ViewBag.time = phase[1] +' '+ phase[2];
+            }
+            catch { }
+            var a = db.Attendances.Where(x => x.Date == date ).ToList();
+            if (a.Count==0)
+                ViewBag.Type = 2;
+            else
+                ViewBag.Type = 1;
+            return View();
+        }
+
+        // POST: Attendances/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,EmployeeId,Time,Date,Type")] Attendance attendance)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    string time = DateTime.Now.ToString();
+                    string[] phase = time.Split(' ');
+                    attendance.Date = phase[0];
+                    attendance.Time = phase[1] +' '+ phase[2];
+                }
+                catch { }
+                db.Attendances.Add(attendance);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Create");
+            }
+
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName", attendance.EmployeeId);
+            return View(attendance);
+        }
+
+        // GET: Attendances/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attendance attendance = await db.Attendances.FindAsync(id);
+            if (attendance == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName", attendance.EmployeeId);
+            return View(attendance);
+        }
+
+        // POST: Attendances/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,EmployeeId,Time,Date,Type")] Attendance attendance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(attendance).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName", attendance.EmployeeId);
+            return View(attendance);
+        }
+
+        // GET: Attendances/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attendance attendance = await db.Attendances.FindAsync(id);
+            if (attendance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(attendance);
+        }
+
+        // POST: Attendances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Attendance attendance = await db.Attendances.FindAsync(id);
+            db.Attendances.Remove(attendance);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ UIUHr/Controllers/EmployeesController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UIUHr.Models;
+using Microsoft.Reporting.WebForms;
+using System.IO;
+
+namespace UIUHr.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private HrDBEntities db = new HrDBEntities();
+
+        // GET: Employees
+        public async Task<ActionResult> Index()
+        {
+            var employees = db.Employees.Include(e => e.JobType);
+            return View(await employees.ToListAsync());
+        }
+
+        public ActionResult Report(string id)
+        {
+            LocalReport lr = new LocalReport();
+            string path = Path.Combine(Server.MapPath("~/Report"), "Report1.rdlc");
+            if (System.IO.File.Exists(path))
+            {
+                lr.ReportPath = path;
+            }
+            else
+            {
+                return View("Index");
+            }
+            List<Employee> cm = new List<Employee>();
+            using (HrDBEntities dc = new HrDBEntities())
+            {
+                cm = dc.Employees.ToList();
+            }
+            ReportDataSource rd = new ReportDataSource("DataSet1", cm);
+            lr.DataSources.Add(rd);
+            string reportType = id;
+            string mimeType;
+            string encoding;
+            string fileNameExtension;
+
+
+
+            string deviceInfo =
+
+            "<DeviceInfo>" +
+            "  <OutputFormat>" + id + "</OutputFormat>" +
+            "  <PageWidth>10in</PageWidth>" +
+            "  <PageHeight>11in</PageHeight>" +
+            "  <MarginTop>0.5in</MarginTop>" +
+            "  <MarginLeft>1in</MarginLeft>" +
+            "  <MarginRight>1in</MarginRight>" +
+            "  <MarginBottom>0.5in</MarginBottom>" +
+            "</DeviceInfo>";
+
+            Warning[] warnings;
+            string[] streams;
+            byte[] renderedBytes;
+
+            renderedBytes = lr.Render(
+                reportType,
+                deviceInfo,
+                out mimeType,
+                out encoding,
+                out fileNameExtension,
+                out streams,
+                out warnings);
+            return File(renderedBytes, mimeType);
+        }
+        // GET: Employees/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = await db.Employees.FindAsync(id);
+            List<Education> education = db.Educations.Where(x => x.EmployeeId == employee.Id).ToList();
+            Address address = db.Addresses.FirstOrDefault(x => x.EmployeeId == employee.Id);
+            ViewBag.edu = education;
+            ViewBag.addr = address;
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            ViewBag.JobTypeId = new SelectList(db.JobTypes, "Id", "Name");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,FirstName,LastName,DOB,Phone,Email,Nationality,NID,PhotoPath,JobTypeId")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+               // return Redirect(Url.Action("Create", "Addresses"));
+            }
+
+            ViewBag.JobTypeId = new SelectList(db.JobTypes, "Id", "Name", employee.JobTypeId);
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.JobTypeId = new SelectList(db.JobTypes, "Id", "Name", employee.JobTypeId);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,FirstName,LastName,DOB,Phone,Email,Nationality,NID,PhotoPath,JobTypeId")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.JobTypeId = new SelectList(db.JobTypes, "Id", "Name", employee.JobTypeId);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Employee employee = await db.Employees.FindAsync(id);
+            db.Employees.Remove(employee);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
